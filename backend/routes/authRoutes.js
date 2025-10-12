fe386df721fa81ff8807aedaeaad2a0d95399afd
vikash <vp946203@gmail.com>
2025-10-12 12:48:27 +0530

    fix some bugs

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -25,13 +25,23 @@ router.post("/signup", async (req, res) => {
     }
 });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-    req.session.user = {
-        id: User._id,
-        username: User.username,
-    };
-    res.json({ message: "Logged in successfully", user: req.user });
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) return next(err);
+        if (!user) return res.status(401).json({ message: info.message || "Invalid credentials" });
+
+        req.login(user, (err) => {
+            if (err) return next(err);
+            req.session.user = {
+                id: User._id,
+                username: User.username,
+            };
+
+            res.json({ message: "Logged in successfully", user });
+        });
+    })(req, res, next);
 });
+
 
 router.get("/logout", (req, res) => {
     req.logout(err => {
